ef974ecbaf1ce2249d0518e7bb66cc75b90431e1
Oliver Richardson <oli@cs.cornell.edu>
2020-03-29 02:55:26 -0400

    giant overhaul

---
+++ buddy/index.php
@@ -0,0 +1,34 @@
+<?php  
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
+	
+	// load twig engine...
+	require_once '../vendor/autoload.php';
+
+	$loader = new \Twig\Loader\FilesystemLoader([ __DIR__ . '/twigs',  __DIR__ . '/../twigs' ]);
+	$twig = new \Twig\Environment($loader, [
+	    // 'cache' => __DIR__ . '/twigs/cache',
+		 // 'auto_reload' => true
+	]);
+	
+	$template = $twig->load('buddy.twig.html');
+	
+	
+	require_once "database.php";
+	require_once "buddies_from_db.php";
+
+	$netid = 'oer5';
+	// if(array_key_exists("NETID", getallheaders())) {
+	// 	$netid = getallheaders()["NETID"];
+		$buds = getBuds($netid);
+		
+		echo $template->render(['buddies' => $buds]);
+		
+		$csgo_db->query("UPDATE Users SET last_online=now() WHERE netid='".$netid."'");
+		$csgo_db->close();
+	// }
+	// else {
+	// 	echo $template->render(['buddies' => []]);
+	// }
+?>
