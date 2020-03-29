e0344fe24f6c00ed097626e49ce92ced3c55e89f
Oliver Richardson <oli@cs.cornell.edu>
2020-03-29 03:52:47 -0400

    no longer uses my netid

--- buddy/index.php
+++ buddy/index.php
@@ -18,17 +18,17 @@
 	require_once "database.php";
 	require_once "buddies_from_db.php";
 
-	$netid = 'oer5';
-	// if(array_key_exists("NETID", getallheaders())) {
-	// 	$netid = getallheaders()["NETID"];
+	// $netid = 'oer5';
+	if(array_key_exists("NETID", getallheaders())) {
+		$netid = getallheaders()["NETID"];
 		$buds = getBuds($netid);
 		
 		echo $template->render(['buddies' => $buds]);
 		
 		$csgo_db->query("UPDATE Users SET last_online=now() WHERE netid='".$netid."'");
 		$csgo_db->close();
-	// }
-	// else {
-	// 	echo $template->render(['buddies' => []]);
-	// }
+	}
+	else {
+		echo $template->render(['buddies' => []]);
+	}
 ?>
