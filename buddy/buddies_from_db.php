ec10fe4f6b4929b938cd932775d383ca8380c9ff
Oliver Richardson <oli@cs.cornell.edu>
2020-03-29 03:43:27 -0400

    no more fetch_all

--- buddy/buddies_from_db.php
+++ buddy/buddies_from_db.php
@@ -5,13 +5,20 @@ error_reporting(E_ALL);
 
 // requires database.php to be loaded for global!
 function getBuds($netid) {
-    global $csgo_db;
+	global $csgo_db;
 	// Create tables
 	$sql = sprintf("SELECT other.fname as fname, other.lname as lname, other.netid as netid, other.last_online as last_online FROM UserCell JOIN Users AS other on other.netid=UserCell.user JOIN UserCell as me on me.cell=UserCell.cell WHERE me.user = '%s'", $netid);
 
 	$rslt = $csgo_db->query($sql) ;
 	var_dump($rslt);
-	return $rslt->fetch_all(MYSQLI_ASSOC);
+
+	$rows = [];
+	while ($row = $result->fetch_assoc()) {
+		array_push($rows, $row->getArrayCopy());
+	}
+	/* free result set */
+	$rslt->free();
+	return $rows;
 }
 
 ?>
