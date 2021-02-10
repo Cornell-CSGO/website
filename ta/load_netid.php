b1f62a12bc372e45dfa66d0a5993feeb256f1d62
Oliver Richardson <oli@cs.cornell.edu>
2021-02-10 15:48:06 -0500

    forgot to replace one instance of NETID with uid

--- ta/load_netid.php
+++ ta/load_netid.php
@@ -1,7 +1,7 @@
 <?php 
 	$netid = NULL;
 	if (array_key_exists("uid", $_SERVER)) {
-		$netid = $_SERVER["NETID"];
+		$netid = $_SERVER["uid"];
 	} elseif (array_key_exists("TEST_NETID", $Opt)) {
 		$netid = $Opt['TEST_NETID'];
 	}
