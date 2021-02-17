9666730d2934d54d57136b61586d8ce7c2aa9498
Oliver Richardson <oli@cs.cornell.edu>
2021-02-16 22:04:06 -0500

    log csv pulling possible

---
+++ ta/admin/log/index.php
@@ -0,0 +1,19 @@
+
+<?php
+
+require "../../database.php";
+require "../../load_netid.php";
+
+if(! in_array($netid, array("oer5"))) {
+ 	throw new Exception("No permission to download form data!"); 
+}
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+global $csgo_db;
+
+require "../table2csv.php";
+
+table2csv($csgo_db, "TAFormLog");
