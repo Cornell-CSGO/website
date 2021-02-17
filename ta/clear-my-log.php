5c4a2a6ee0100373707751a3d802e28c0cbb7ace
Oliver Richardson <oli@cs.cornell.edu>
2021-02-16 22:06:44 -0500

    disabling clear

--- ta/clear-my-log.php
+++ ta/clear-my-log.php
@@ -3,8 +3,10 @@ require_once "database.php";
 require_once "load_netid.php";
 
 
-if($rslt = $csgo_db->query(<<<SQL
-DELETE FROM TAFormLog WHERE netid = "$netid";
-SQL)) {
-	echo "<h2>Purge Successful for netid <code>$netid</code></h2>";
-}
+echo "<h1> This toy feature is disabled to prevent multiple submission :( </h1>";
+
+// if($rslt = $csgo_db->query(<<<SQL
+// DELETE FROM TAFormLog WHERE netid = "$netid";
+//SQL)) {
+// 	echo "<h2>Purge Successful for netid <code>$netid</code></h2>";
+// }
